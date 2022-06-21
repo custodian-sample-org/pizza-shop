5334f7e002692c7f02305a3d369fd834eb88c2dc
Morgante Pell <morgantep@google.com>
2022-06-20 23:26:02 -0700

    Add some console.log statements

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 
+import {DEBUG} from '../constants'
+
 function MyApp({ Component, pageProps }) {
+  if (DEBUG) {
+    console.log("This is okay, since it is gated in a DEBUG.");
+  }
   return <Component {...pageProps} />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  console.log("This should be removed.");
   return (
     <div className={styles.container}>
       <Head>
