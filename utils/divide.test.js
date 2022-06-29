2f3391a0d8b6e603323be8a7324eb7e4da9a5731
Morgante Pell <morgantep@google.com>
2022-06-29 12:53:31 -0700

    Add some jest tests

---
+++ utils/divide.test.js
@@ -0,0 +1,9 @@
+const divide = require('./divide');
+
+test('divides 4 by 2 to equal 2', () => {
+  expect(divide(4, 2)).toBe(2);
+});
+
+test('divides -4 by 2 to equal -2', () => {
+  expect(divide(-4, 2)).toBe(-2);
+});
